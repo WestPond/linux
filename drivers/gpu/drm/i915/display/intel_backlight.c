3837a209f6439143a2b8d3b93d11e2f44fdcfbee
Nathan Ford <nford@westpond.com>
2025-08-28 13:24:04 -0400

    Updated thinclient patches for kernel 6.12
    Signed-off-by: Nathan Ford <nford@westpond.com>

--- drivers/gpu/drm/i915/display/intel_backlight.c
+++ drivers/gpu/drm/i915/display/intel_backlight.c
@@ -437,7 +437,8 @@ void intel_backlight_disable(const struct drm_connector_state *old_conn_state)
 	struct drm_i915_private *i915 = to_i915(connector->base.dev);
 	struct intel_panel *panel = &connector->panel;
 
-	if (!panel->backlight.present)
+//  This crashes on soft boot. Seems the driver cannot handle the GPU being in a warm state correctly
+//	if (!panel->backlight.present)
 		return;
 
 	/*
